1fceea677769e3dffc5f968e0a9b0ccdb44ee94f
Fish Chen <fish.chen@onramplab.com>
2025-02-21 17:23:40 +0800

    test new version

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// 設定首頁路由
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, World! Test Verstion 3")
+		fmt.Fprintln(w, "Hello, World! Test Verstion 4")
 	})
 
 	// 啟動伺服器，監聽在 8080 埠
